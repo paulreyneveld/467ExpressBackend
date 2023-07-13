75148beb54b6eeffe25b20744c4dd14067a85c9e
Eric Chin <47379956+eyc94@users.noreply.github.com>
2023-07-12 18:04:23 -0700

    Pet route to create a pet (#4)

--- controllers/pets.js
+++ controllers/pets.js
@@ -1,7 +1,7 @@
 const petsRouter = require('express').Router();
 const petModel = require('../models/pet');
 
-const { getAllPets, getPet, putPet, patchPet, deletePet } = petModel;
+const { createPet, getAllPets, getPet, putPet, patchPet, deletePet } = petModel;
 
 // TODO: GET all pets. Also look into pagination, filtering, and searching.
 petsRouter.get('/', (req, res) => {
@@ -17,9 +17,26 @@ petsRouter.get('/:id', (req, res) => {
 });
 
 // TODO: CREATE a pet.
-petsRouter.post('/', (req, res) => {
+petsRouter.post('/', async (req, res) => {
   // Admin: Create a pet.
   // Public: No access.
+  const newPet = {
+    typeAnimal: req.body.typeAnimal,
+    breed: req.body.breed,
+    description: req.body.description,
+    images: req.body.images,
+    goodWithAnimals: req.body.goodWithAnimals,
+    goodWithChildren: req.body.goodWithChildren,
+    leashedAllTimes: req.body.leashedAllTimes,
+    availability: 'Available',
+    creationDate: new Date().toISOString(),
+  };
+
+  const entity = await createPet(newPet);
+
+  const result = entity.data;
+  result.id = entity.key.id;
+  return res.json(result);
 });
 
 module.exports = petsRouter;
