525364fd305fe9581c6095634e7e5a4cdeaaa87f
Eric Chin <47379956+eyc94@users.noreply.github.com>
2023-07-12 18:21:42 -0700

    Pet route to get a pet (#6)

--- models/pet.js
+++ models/pet.js
@@ -21,7 +21,15 @@ const getAllPets = async () => {
   return entities[0].map(ds.fromDatastore);
 };
 
-const getPet = async () => {};
+const getPet = async (id) => {
+  const key = datastore.key([PET, parseInt(id, 10)]);
+  const entity = await datastore.get(key);
+  if (entity[0] === undefined || entity[0] === null) {
+    return entity;
+  }
+
+  return entity.map(ds.fromDatastore);
+};
 
 const putPet = async () => {};
 
