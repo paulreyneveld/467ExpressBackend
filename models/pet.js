522c7283c6bf063d714be31849d51c575d1bd2d4
Eric Chin <47379956+eyc94@users.noreply.github.com>
2023-07-12 17:15:57 -0700

    Created models skeleton for entities (#3)

---
+++ models/pet.js
@@ -0,0 +1,20 @@
+const createPet = async () => {};
+
+const getAllPets = async () => {};
+
+const getPet = async () => {};
+
+const putPet = async () => {};
+
+const patchPet = async () => {};
+
+const deletePet = async () => {};
+
+module.exports = {
+  createPet,
+  getAllPets,
+  getPet,
+  putPet,
+  patchPet,
+  deletePet,
+};
---
+++ models/user.js
@@ -0,0 +1,20 @@
+const createUser = async () => {};
+
+const getAllUsers = async () => {};
+
+const getUser = async () => {};
+
+const putUser = async () => {};
+
+const patchUser = async () => {};
+
+const deleteUser = async () => {};
+
+module.exports = {
+  createUser,
+  getAllUsers,
+  getUser,
+  putUser,
+  patchUser,
+  deleteUser,
+};
