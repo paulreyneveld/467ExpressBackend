3364cd0e49c560e52e57391d1c2c8e52374bb729
Eric Chin <47379956+eyc94@users.noreply.github.com>
2023-07-19 20:12:18 -0700

    Added input validation for create pet route (#16)

--- utils/config.js
+++ utils/config.js
@@ -1,5 +1,17 @@
 const PORT = process.env.PORT || 3001;
 
+const validAnimalTypes = ['dog', 'cat', 'other'];
+const validBreeds = ['german shepherd', 'other'];
+const availabilityOptions = [
+  'Available',
+  'Not Available',
+  'Pending',
+  'Adopted',
+];
+
 module.exports = {
   PORT,
+  validAnimalTypes,
+  validBreeds,
+  availabilityOptions,
 };
