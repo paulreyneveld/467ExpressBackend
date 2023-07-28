4910f7bcfbbf787686a24156b564523269a97158
Eric Chin <ericychin94@gmail.com>
2023-07-28 12:03:00 -0700

    Added more valid breeds

--- utils/config.js
+++ utils/config.js
@@ -1,7 +1,20 @@
 const PORT = process.env.PORT || 3001;
 
 const validAnimalTypes = ['dog', 'cat', 'other'];
-const validBreeds = ['german shepherd', 'other'];
+const validBreeds = [
+  'german shepherd',
+  'Labrador Retriever',
+  'German Shepherd',
+  'Golden Retriever',
+  'Bulldog',
+  'Poodle',
+  'Beagle',
+  'Rottweiler',
+  'Yorkshire Terrier',
+  'Dachshund',
+  'Boxer',
+  'other'
+];
 const availabilityOptions = [
   'available',
   'not available',
